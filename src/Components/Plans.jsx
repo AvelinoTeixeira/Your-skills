c9abae97c6c01989300e0bf5710dd3de5b8e142d
AvelinoTeixeira <129909440+AvelinoTeixeira@users.noreply.github.com>
2023-12-17 13:45:16 +0100

    Hi

--- src/Components/Plans.jsx
+++ src/Components/Plans.jsx
@@ -4,42 +4,44 @@ function Plans() {
   return (
     <div className='plans'>
       <section>
-      <div className='elements'>
+      <div className='title'>
         <h1>Here are all our plans</h1>
         <p>On the other hand, we dwnounce with righteous indignation and dislike men who are so
                                             beguiled demoralized</p>
-          <div className='start'>
-          <h1>Start</h1>
-          <p>On the other hand, we dwnounce <br /> with righteous indignation and dislike</p>
-          <h3><strong>50$</strong> /month</h3>
-          <a href="#">Videos of lessons</a>
-            <a href="#">Homework check</a>
-            <a href="#">Additional practical tasks</a>
-            <a href="#">Monthly conferences</a>
-            <a href="#">Personal advice from teachers</a>
-            <button className='button'>Get Started</button>
-          </div>
-          <div className='advanced'>
-            <h1>Advanced</h1>
+          <div className="elements">
+            <div className='start'>
+            <h1>Start</h1>
             <p>On the other hand, we dwnounce <br /> with righteous indignation and dislike</p>
-          <h3><strong>75$</strong> /month</h3>
-            <a href="#"> Videos of lessons</a>
-            <a href="#">Homework check</a>
-            <a href="#">Additional practical tasks</a>
-            <a href="#">Monthly conferences</a>
-            <a href="#">Personal advice from teachers</a>
-            <button className='button'>Get Started</button>
-          </div>
-          <div className='premium'>
-            <h1>Premium</h1>
-            <p>On the other hand, we dwnounce <br /> with righteous indignation and dislike</p>
-            <h3><strong>100$</strong> /month</h3>
+            <h3><strong>50$</strong> /month</h3>
             <a href="#">Videos of lessons</a>
-            <a href="#">Homework check</a>
-            <a href="#">Additional practical tasks</a>
-            <a href="#">Monthly conferences</a>
-            <a href="#">Personal advice from teachers</a>
-            <button className='button'>Get Started</button>
+              <a href="#">Homework check</a>
+              <a href="#">Additional practical tasks</a>
+              <a href="#">Monthly conferences</a>
+              <a href="#">Personal advice from teachers</a>
+              <button className='buttonsem'>Get Started</button>
+            </div>
+            <div className='advanced'>
+              <h1>Advanced</h1>
+              <p>On the other hand, we dwnounce <br /> with righteous indignation and dislike</p>
+            <h3><strong>75$</strong> /month</h3>
+              <a href="#"> Videos of lessons</a>
+              <a href="#">Homework check</a>
+              <a href="#">Additional practical tasks</a>
+              <a href="#">Monthly conferences</a>
+              <a href="#">Personal advice from teachers</a>
+              <button className='button'>Get Started</button>
+            </div>
+            <div className='premium'>
+              <h1>Premium</h1>
+              <p>On the other hand, we dwnounce <br /> with righteous indignation and dislike</p>
+              <h3><strong>100$</strong> /month</h3>
+              <a href="#">Videos of lessons</a>
+              <a href="#">Homework check</a>
+              <a href="#">Additional practical tasks</a>
+              <a href="#">Monthly conferences</a>
+              <a href="#">Personal advice from teachers</a>
+              <button className='buttonsem'>Get Started</button>
+            </div>
           </div>
         </div>
       </section>
