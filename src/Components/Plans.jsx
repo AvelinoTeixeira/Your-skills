80b36e3068baa77b36376e62a90fa65b4471cff5
AvelinoTeixeira <129909440+AvelinoTeixeira@users.noreply.github.com>
2023-12-18 23:23:53 +0100

    Hi

--- src/Components/Plans.jsx
+++ src/Components/Plans.jsx
@@ -4,11 +4,11 @@ function Plans() {
   return (
     <div className='plans'>
       <section>
-      <div className='title'>
-        <h1>Here are all our plans</h1>
-        <p>On the other hand, we dwnounce with righteous indignation and dislike men who are so
-                                            beguiled demoralized</p>
-          <div className="elements">
+        <div className='title'>
+          <h1>Here are all our plans</h1>
+          <p>On the other hand, we dwnounce with righteous indignation and dislike men who are sobeguiled demoralized</p>
+        </div>
+        <div className="elements">
             <div className='start'>
              <h1>Start</h1>
              <p>On the other hand, we dwnounce <br /> with righteous indignation and dislike</p>
@@ -55,15 +55,23 @@ function Plans() {
               <h1>Premium</h1>
               <p>On the other hand, we dwnounce <br /> with righteous indignation and dislike</p>
               <h3><strong>100$</strong> /month</h3>
-              <a href="#">Videos of lessons</a>
-              <a href="#">Homework check</a>
-              <a href="#">Additional practical tasks</a>
-              <a href="#">Monthly conferences</a>
-              <a href="#">Personal advice from teachers</a>
-              <button className='buttonsem'>Get Started</button>
+              <div className="circles">
+               <div className="circle">&#10003;</div>
+               <div className="circle">&#10003;</div>
+               <div className="red-icon">&#10007;</div>
+               <div className="red-icon">&#10007;</div>
+               <div className="red-icon">&#10007;</div>
+             </div>
+              <div className="item">
+                <a href="#">Videos of lessons</a>
+                <a href="#">Homework check</a>
+                <a href="#">Additional practical tasks</a>
+                <a href="#">Monthly conferences</a>
+                <a href="#">Personal advice from teachers</a>
+                <button className='buttonsem'>Get Started</button>
+              </div>
             </div>
           </div>
-        </div>
       </section>
     </div>
   )
