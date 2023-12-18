454f7dd10183a326c8dffc475766488a2daa4b2a
AvelinoTeixeira <129909440+AvelinoTeixeira@users.noreply.github.com>
2023-12-18 19:49:42 +0100

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,18 +10,7 @@ function Home() {
 
   /** Menu  */
   // Menu
-  let menu = document.querySelector('#menu-icon');
-  let nav = document.querySelector('.nav')
   
-  menu.onclick = () => {
-      menu.classList.toggle('bx-x')
-      nav.classList.toggle('active')
-  }
-
-  window.onscroll = () => {
-    menu.classList.remove('bx-x');
-    nav.classList.remove('active');
-}
 
   return (
     <body>
