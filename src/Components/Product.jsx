83145bf2110618ec07b56c9bb0689fa4ba33da67
AvelinoTeixeira <129909440+AvelinoTeixeira@users.noreply.github.com>
2023-12-18 20:49:47 +0100

    Hi

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -7,19 +7,20 @@ function Product() {
   return (
     <div className='product'>
       <section>
-        <div className='many'>
-        <img src={Saly5} alt="" />
-          <h1>We have been <br /> improving our product <br /><span>for many years</span></h1>
-          <p>On the other hand, we dwnounce with righteous indignation <br /> and dislike men who are so beguiled and demoralized</p>
-          <button  className='button'>Get Started</button>
-        </div>
-
-          <div className='time'>
-            <img src={Saly6} alt="" />
-            <h1>Your can practice at any <br /> <span>time convenient for you</span></h1>
+        <div className='images'>
+          <div className='many'>
+          <img src={Saly5} alt="" />
+            <h1>We have been <br /> improving our product <br /><span>for many years</span></h1>
             <p>On the other hand, we dwnounce with righteous indignation <br /> and dislike men who are so beguiled and demoralized</p>
-            <button className='button'>Get Started</button>
+            <button  className='button'>Get Started</button>
           </div>
+            <div className='time'>
+              <img src={Saly6} alt="" />
+              <h1>Your can practice at any <br /> <span>time convenient for you</span></h1>
+              <p>On the other hand, we dwnounce with righteous indignation <br /> and dislike men who are so beguiled and demoralized</p>
+              <button className='button'>Get Started</button>
+            </div>
+        </div>
       </section>
     </div>
   )
