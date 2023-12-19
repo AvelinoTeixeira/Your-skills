c513b8671cbd1c629db2eea418a6642575e1356e
AvelinoTeixeira <129909440+AvelinoTeixeira@users.noreply.github.com>
2023-12-19 01:35:53 +0100

    Hi

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,11 +8,34 @@ function Footer () {
         <h1>Developer</h1>
           <p>Bui must explain to you how all this  mistaken idea of denouncing <br /> pleassure and praising <br />
           pain was born and will give you a complete account of the system</p>
-          
-          <div className='buttons'>
-            <button className="button1">Get Started</button>
-            <button className="button2">Discount</button>
+          <div className='Plataform'>
+            <h3>Plataform</h3>
+            <a href="#">Overview</a>
+            <a href="#">Features</a>
+            <a href="#">About</a>
+            <a href="#">Pricing</a>
           </div>
+          <div className='Help'>
+            <h3>Help</h3>
+            <a href="#">Hows does it work?</a>
+            <a href="#">Where to ask questions?</a>
+            <a href="#">How to play?</a>
+            <a href="#">What is needed for this?</a>
+          </div>
+          <div className='Contacts'>
+          <h3>Contacts</h3>
+            <a href="#">(716)532-3577</a>
+            <a href="#">116 Archer Rd</a>
+            <a href="#">Broadalbin</a>
+            <a href="#">New You(NY)</a>
+            <a href="#">12025</a>
+          </div>
+          <hr />
+          <div className='buttons'>
+            <button className="buttonY">Your email</button>
+            <button className="buttonS">Subscribe</button>
+          </div>
+        
       </div>
     </footer>
   )
